78a36638337fa130923572670b94e5b1233bb5af
dashooook <104925691+dashooook@users.noreply.github.com>
2022-05-04 22:18:31 +0300

    Update AutoGoPrize.lua

--- AutoGoPrize.lua
+++ AutoGoPrize.lua
@@ -1,6 +1,6 @@
 script_name("AutoGoPrize")
 script_author("Dashok.")
-script_version('1.1')
+script_version('1.2')
 
 require("lib.moonloader")
 
@@ -35,66 +35,50 @@ function theme_white() -- Rice Style
     local colors = style.Colors
     local clr = imgui.Col
     local ImVec4 = imgui.ImVec4
-    local ImVec2 = imgui.ImVec2
-
-    style.WindowPadding = imgui.ImVec2(8, 8)
-    style.WindowRounding = 6
-    style.ChildWindowRounding = 5
-    style.FramePadding = imgui.ImVec2(5, 3)
-    style.FrameRounding = 3.0
-    style.ItemSpacing = imgui.ImVec2(5, 4)
-    style.ItemInnerSpacing = imgui.ImVec2(4, 4)
-    style.IndentSpacing = 21
-    style.ScrollbarSize = 10.0
-    style.ScrollbarRounding = 13
-    style.GrabMinSize = 8
-    style.GrabRounding = 1
-    style.WindowTitleAlign = imgui.ImVec2(0.5, 0.5)
-    style.ButtonTextAlign = imgui.ImVec2(0.5, 0.5)
-
-        colors[clr.Text]                   = ImVec4(0.00, 0.00, 0.00, 1.00);
-        colors[clr.TextDisabled]           = ImVec4(0.50, 0.50, 0.50, 1.00);
-        colors[clr.WindowBg]               = ImVec4(0.86, 0.86, 0.86, 1.00);
-        colors[clr.ChildWindowBg]          = ImVec4(0.71, 0.71, 0.71, 1.00);
-        colors[clr.PopupBg]                = ImVec4(0.79, 0.79, 0.79, 1.00);
-        colors[clr.Border]                 = ImVec4(0.00, 0.00, 0.00, 0.36);
-        colors[clr.BorderShadow]           = ImVec4(0.00, 0.00, 0.00, 0.10);
-        colors[clr.FrameBg]                = ImVec4(1.00, 1.00, 1.00, 1.00);
-        colors[clr.FrameBgHovered]         = ImVec4(1.00, 1.00, 1.00, 1.00);
-        colors[clr.FrameBgActive]          = ImVec4(1.00, 1.00, 1.00, 1.00);
-        colors[clr.TitleBg]                = ImVec4(1.00, 1.00, 1.00, 0.81);
-        colors[clr.TitleBgActive]          = ImVec4(1.00, 1.00, 1.00, 1.00);
-        colors[clr.TitleBgCollapsed]       = ImVec4(1.00, 1.00, 1.00, 0.51);
-        colors[clr.MenuBarBg]              = ImVec4(1.00, 1.00, 1.00, 1.00);
-        colors[clr.ScrollbarBg]            = ImVec4(1.00, 1.00, 1.00, 0.86);
-        colors[clr.ScrollbarGrab]          = ImVec4(0.37, 0.37, 0.37, 1.00);
-        colors[clr.ScrollbarGrabHovered]   = ImVec4(0.60, 0.60, 0.60, 1.00);
-        colors[clr.ScrollbarGrabActive]    = ImVec4(0.21, 0.21, 0.21, 1.00);
-        colors[clr.ComboBg]                = ImVec4(0.61, 0.61, 0.61, 1.00);
-        colors[clr.CheckMark]              = ImVec4(0.42, 0.42, 0.42, 1.00);
-        colors[clr.SliderGrab]             = ImVec4(0.51, 0.51, 0.51, 1.00);
-        colors[clr.SliderGrabActive]       = ImVec4(0.65, 0.65, 0.65, 1.00);
-        colors[clr.Button]                 = ImVec4(0.52, 0.52, 0.52, 0.83);
-        colors[clr.ButtonHovered]          = ImVec4(0.58, 0.58, 0.58, 0.83);
-        colors[clr.ButtonActive]           = ImVec4(0.44, 0.44, 0.44, 0.83);
-        colors[clr.Header]                 = ImVec4(0.65, 0.65, 0.65, 1.00);
-        colors[clr.HeaderHovered]          = ImVec4(0.73, 0.73, 0.73, 1.00);
-        colors[clr.HeaderActive]           = ImVec4(0.53, 0.53, 0.53, 1.00);
-        colors[clr.Separator]              = ImVec4(0.46, 0.46, 0.46, 1.00);
-        colors[clr.SeparatorHovered]       = ImVec4(0.45, 0.45, 0.45, 1.00);
-        colors[clr.SeparatorActive]        = ImVec4(0.45, 0.45, 0.45, 1.00);
-        colors[clr.ResizeGrip]             = ImVec4(0.23, 0.23, 0.23, 1.00);
-        colors[clr.ResizeGripHovered]      = ImVec4(0.32, 0.32, 0.32, 1.00);
-        colors[clr.ResizeGripActive]       = ImVec4(0.14, 0.14, 0.14, 1.00);
-        colors[clr.CloseButton]            = ImVec4(0.40, 0.39, 0.38, 0.16);
-        colors[clr.CloseButtonHovered]     = ImVec4(0.40, 0.39, 0.38, 0.39);
-        colors[clr.CloseButtonActive]      = ImVec4(0.40, 0.39, 0.38, 1.00);
-        colors[clr.PlotLines]              = ImVec4(0.61, 0.61, 0.61, 1.00);
-        colors[clr.PlotLinesHovered]       = ImVec4(1.00, 1.00, 1.00, 1.00);
-        colors[clr.PlotHistogram]          = ImVec4(0.70, 0.70, 0.70, 1.00);
-        colors[clr.PlotHistogramHovered]   = ImVec4(1.00, 1.00, 1.00, 1.00);
-        colors[clr.TextSelectedBg]         = ImVec4(0.62, 0.62, 0.62, 1.00);
-        colors[clr.ModalWindowDarkening]   = ImVec4(0.26, 0.26, 0.26, 0.60);
+    
+    colors[clr.Text]                   = ImVec4(0.01, 0.36, 1.00, 1.00);
+    colors[clr.TextDisabled]           = ImVec4(0.00, 0.60, 0.67, 0.97);
+    colors[clr.WindowBg]               = ImVec4(0.02, 0.00, 0.06, 1.00);
+    colors[clr.ChildWindowBg]          = ImVec4(0.09, 0.01, 0.15, 0.26);
+    colors[clr.PopupBg]                = ImVec4(0.00, 0.00, 0.00, 1.00);
+    colors[clr.Border]                 = ImVec4(0.07, 0.10, 0.15, 0.56);
+    colors[clr.BorderShadow]           = ImVec4(0.00, 0.00, 0.00, 0.49);
+    colors[clr.FrameBg]                = ImVec4(0.06, 0.19, 0.46, 0.29);
+    colors[clr.FrameBgHovered]         = ImVec4(0.03, 0.00, 0.06, 0.22);
+    colors[clr.FrameBgActive]          = ImVec4(0.00, 0.00, 0.00, 0.10);
+    colors[clr.TitleBg]                = ImVec4(0.01, 0.01, 0.05, 1.00);
+    colors[clr.TitleBgActive]          = ImVec4(0.14, 0.26, 0.55, 1.00);
+    colors[clr.TitleBgCollapsed]       = ImVec4(0.40, 0.40, 0.90, 0.20);
+    colors[clr.MenuBarBg]              = ImVec4(0.00, 0.00, 0.00, 0.80);
+    colors[clr.ScrollbarBg]            = ImVec4(0.27, 0.00, 1.00, 0.19);
+    colors[clr.ScrollbarGrab]          = ImVec4(0.00, 1.00, 0.95, 0.30);
+    colors[clr.ScrollbarGrabHovered]   = ImVec4(0.00, 0.00, 0.00, 0.40);
+    colors[clr.ScrollbarGrabActive]    = ImVec4(0.02, 0.98, 1.00, 0.40);
+    colors[clr.ComboBg]                = ImVec4(0.00, 0.00, 0.00, 0.99);
+    colors[clr.CheckMark]              = ImVec4(0.00, 0.58, 1.00, 1.00);
+    colors[clr.SliderGrab]             = ImVec4(1.00, 1.00, 1.00, 0.30);
+    colors[clr.SliderGrabActive]       = ImVec4(0.80, 0.50, 0.50, 1.00);
+    colors[clr.Button]                 = ImVec4(0.09, 0.06, 0.20, 1.00);
+    colors[clr.ButtonHovered]          = ImVec4(0.08, 0.03, 0.21, 0.27);
+    colors[clr.ButtonActive]           = ImVec4(0.00, 0.54, 1.00, 1.00);
+    colors[clr.Header]                 = ImVec4(0.35, 0.02, 1.00, 0.45);
+    colors[clr.HeaderHovered]          = ImVec4(0.06, 0.39, 0.40, 0.80);
+    colors[clr.HeaderActive]           = ImVec4(0.00, 0.86, 1.00, 0.80);
+    colors[clr.Separator]              = ImVec4(0.07, 0.30, 0.52, 1.00);
+    colors[clr.SeparatorHovered]       = ImVec4(0.00, 0.00, 0.00, 1.00);
+    colors[clr.SeparatorActive]        = ImVec4(0.06, 0.06, 0.90, 1.00);
+    colors[clr.ResizeGrip]             = ImVec4(0.02, 0.01, 0.27, 0.30);
+    colors[clr.ResizeGripHovered]      = ImVec4(0.24, 0.00, 0.87, 0.60);
+    colors[clr.ResizeGripActive]       = ImVec4(0.00, 0.00, 0.00, 0.90);
+    colors[clr.CloseButton]            = ImVec4(0.00, 0.00, 0.00, 0.90);
+    colors[clr.CloseButtonHovered]     = ImVec4(1.00, 0.16, 0.00, 0.26);
+    colors[clr.CloseButtonActive]      = ImVec4(1.00, 0.05, 0.05, 1.00);
+    colors[clr.PlotLines]              = ImVec4(0.45, 0.00, 0.73, 1.00);
+    colors[clr.PlotLinesHovered]       = ImVec4(0.07, 0.02, 0.39, 1.00);
+    colors[clr.PlotHistogram]          = ImVec4(0.06, 0.05, 0.12, 1.00);
+    colors[clr.PlotHistogramHovered]   = ImVec4(0.10, 0.06, 0.27, 1.00);
+    colors[clr.TextSelectedBg]         = ImVec4(0.17, 0.06, 0.41, 0.35);
+    colors[clr.ModalWindowDarkening]   = ImVec4(0.28, 0.05, 0.59, 0.35);
 end
 theme_white()
 
@@ -102,16 +86,16 @@ function imgui.OnDrawFrame()
     if main_window.v then
         imgui.SetNextWindowPos(imgui.ImVec2(sw / 2, sh / 2), imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))
         imgui.SetNextWindowSize(imgui.ImVec2(300, 250), imgui.Cond.FirstUseEver)
-    imgui.Begin(u8'Настройки')
-    if imgui.InputText(u8"Задержка (мс)", buffer_time) then
+    imgui.Begin(u8'ГЌГ Г±ГІГ°Г®Г©ГЄГЁ')
+    if imgui.InputText(u8"Г‡Г Г¤ГҐГ°Г¦ГЄГ  (Г¬Г±)", buffer_time) then
     ini.setting.delayTime = buffer_time.v
       inicfg.save(ini)
     end
     imgui.Text("")
-    imgui.CenterText(u8"За сессию вы получили: ")
-    imgui.CenterText(u8"За все время вы получили: ")
+    imgui.CenterText(u8"Г‡Г  Г±ГҐГ±Г±ГЁГѕ ГўГ» ГЇГ®Г«ГіГ·ГЁГ«ГЁ: ")
+    imgui.CenterText(u8"Г‡Г  ГўГ±ГҐ ГўГ°ГҐГ¬Гї ГўГ» ГЇГ®Г«ГіГ·ГЁГ«ГЁ: ")
 
-    if imgui.Button(u8"Закрыть") then
+    if imgui.Button(u8"Г‡Г ГЄГ°Г»ГІГј") then
         main_window.v = false
     end
     imgui.SameLine()
@@ -119,7 +103,7 @@ function imgui.OnDrawFrame()
       thisScript():reload()
     end
     imgui.SameLine()
-    if imgui.Button(u8"Выключить") then
+    if imgui.Button(u8"Г‚Г»ГЄГ«ГѕГ·ГЁГІГј") then
       thisScript():unload()
     end
 
@@ -132,7 +116,7 @@ function msg(text)
 end
 
 function sampev.onServerMessage(color, text)
-    local ptr = "РАЗДАЧА ЗАПУЩЕНА!"
+    local ptr = "ГђГЂГ‡Г„ГЂГ—ГЂ Г‡ГЂГЏГ“Г™Г…ГЌГЂ!"
     if text:find(ptr) then
         sampSendChat("/goprize")
     end
@@ -141,7 +125,7 @@ end
 function sampev.onShowDialog(dialogid, style, title, b1, b2, text)
 
 text = text:gsub("{......}", "")
-local pattern = "Введите данный код в поле внизу: (.+)"
+local pattern = "Г‚ГўГҐГ¤ГЁГІГҐ Г¤Г Г­Г­Г»Г© ГЄГ®Г¤ Гў ГЇГ®Г«ГҐ ГўГ­ГЁГ§Гі: (.+)"
 if text:find(pattern) then
      lua_thread.create(function()
     local n1 = text:match(pattern)
@@ -156,7 +140,7 @@ if dialogid == 7986 then
     local count = -1
     for line in text:gmatch("[^\n]+") do
         count = count + 1
-        if line:find("Получить приз") then
+        if line:find("ГЏГ®Г«ГіГ·ГЁГІГј ГЇГ°ГЁГ§") then
              sampSendDialogResponse(dialogid, 1, count, -1)
              break
         end
@@ -173,9 +157,9 @@ function main()
    
     buffer_time.v = ini.setting.delayTime
 
-   msg("Загружен")
+   msg("Г‡Г ГЈГ°ГіГ¦ГҐГ­")
    msg("Autor: Dashok.")
-   msg("Активация - Автоматическая")
+   msg("ГЂГЄГІГЁГўГ Г¶ГЁГї - ГЂГўГІГ®Г¬Г ГІГЁГ·ГҐГ±ГЄГ Гї")
 
    sampRegisterChatCommand("g.set",sets)
 
@@ -205,7 +189,7 @@ function update()
           lua_thread.create(goupdate) 
         else 
           update = false 
-          msg('У вас и так последняя версия скрипта! Отменяю обновление.')
+          msg('Г“ ГўГ Г± ГЁ ГІГ ГЄ ГЇГ®Г±Г«ГҐГ¤Г­ГїГї ГўГҐГ°Г±ГЁГї Г±ГЄГ°ГЁГЇГІГ ! ГЋГІГ¬ГҐГ­ГїГѕ Г®ГЎГ­Г®ГўГ«ГҐГ­ГЁГҐ.')
         end
       end
     end
@@ -214,14 +198,14 @@ end)
 end
 
 function goupdate()
-msg('Обнаружено новое обновление')
-msg('Текущая версия: '..thisScript().version.." Новая версия: "..version)
+msg('ГЋГЎГ­Г Г°ГіГ¦ГҐГ­Г® Г­Г®ГўГ®ГҐ Г®ГЎГ­Г®ГўГ«ГҐГ­ГЁГҐ')
+msg('Г’ГҐГЄГіГ№Г Гї ГўГҐГ°Г±ГЁГї: '..thisScript().version.." ГЌГ®ГўГ Гї ГўГҐГ°Г±ГЁГї: "..version)
 wait(3000)
 downloadUrlToFile(updatelink, thisScript().path, function(id3, status1, p13, p23)
   if status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
   wait(3000)
-  msg('Обновление завершено!')
-  msg('Ознакомится с ним можно в группе скрипта')
+  msg('ГЋГЎГ­Г®ГўГ«ГҐГ­ГЁГҐ Г§Г ГўГҐГ°ГёГҐГ­Г®!')
+  msg('ГЋГ§Г­Г ГЄГ®Г¬ГЁГІГ±Гї Г± Г­ГЁГ¬ Г¬Г®Г¦Г­Г® Гў ГЈГ°ГіГЇГЇГҐ Г±ГЄГ°ГЁГЇГІГ ')
   wait(4000)
   thisScript():reload()
 end
